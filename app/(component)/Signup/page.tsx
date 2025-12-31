51e3ca38c069c0429f984999f3c8f3e2648a14ac
ZforZaher <mrzaher775@gmail.com>
2025-12-31 21:23:40 +0200

    Login and Signup height fixed again

--- app/(component)/Signup/page.tsx
+++ app/(component)/Signup/page.tsx
@@ -51,130 +51,132 @@ export default function Signup() {
   }
 
   return (
-    <div className=" w-[90%] lg:w-[50%] md:w-[70%] sm:w-[80%] h-screen mx-auto mt-25 border border-[#787878] fade-in">
-      <h1 className="font-mono font-bold text-[#FFFFE3] uppercase text-3xl text-center pt-3">
-        join ZÉID now
-      </h1>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(HandleRegister)} className="py-3">
-          {/* NAME INPUT */}
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem className="px-3">
-                <FormLabel />
-                <FormControl>
-                  <Input
-                    type="text"
-                    placeholder="NAME"
-                    {...field}
-                    className="rounded-none font-semibold  text-[#FFFFE3] outline-[#FFFFE3] text-center"
-                  />
-                </FormControl>
-                <FormDescription />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/* EMAIL */}
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem className="px-3">
-                <FormLabel />
-                <FormControl>
-                  <Input
-                    type="email"
-                    placeholder="EMAIL"
-                    {...field}
-                    className="rounded-none font-semibold  text-[#FFFFE3] outline-[#FFFFE3] text-center"
-                  />
-                </FormControl>
-                <FormDescription />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/* PASSWORD */}
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem className="px-3">
-                <FormLabel />
-                <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="PASSWORD"
-                    {...field}
-                    className="rounded-none font-semibold  text-[#FFFFE3] outline-[#FFFFE3] text-center"
-                  />
-                </FormControl>
-                <FormDescription />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/* REPASSWORD */}
-          <FormField
-            control={form.control}
-            name="rePassword"
-            render={({ field }) => (
-              <FormItem className="px-3">
-                <FormLabel />
-                <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="REPASSWORD"
-                    {...field}
-                    className="rounded-none font-semibold  text-[#FFFFE3] outline-[#FFFFE3] text-center"
-                  />
-                </FormControl>
-                <FormDescription />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/* PHONE */}
-          <FormField
-            control={form.control}
-            name="phone"
-            render={({ field }) => (
-              <FormItem className="px-3">
-                <FormLabel />
-                <FormControl>
-                  <Input
-                    type="phone"
-                    placeholder="PHONE"
-                    {...field}
-                    className="rounded-none font-semibold  text-[#FFFFE3] outline-[#FFFFE3] text-center"
-                  />
-                </FormControl>
-                <FormDescription />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="px-3">
-            <Button
-              type="submit"
-              className="rounded-none font-mono text-base sm:text-lg lg:text-xl font-bold w-full mt-2 sm:mt-2 py-2 sm:py-5 text-center bg-[#EA301F] text-[#FFFFE3] cursor-pointer hover:bg-[#FFFFE3] hover:text-[#000000] transition-all duration-400 uppercase "
-            >
-              Submit
-            </Button>
-          </div>
-          <div>
-            <p className="font-mono capitalize text-sm text-[#FFFFE3] text-center mt-2">
-              already have an account?{" "}
-              <Link href="/Login" className="text-[#EA301F]">
-                Login
-              </Link>
-            </p>
-          </div>
-        </form>
-      </Form>
+    <div className="h-screen">
+      <div className=" w-[90%] lg:w-[50%] md:w-[70%] sm:w-[80%] h-screen mx-auto mt-25 border border-[#787878] fade-in">
+        <h1 className="font-mono font-bold text-[#FFFFE3] uppercase text-3xl text-center pt-3">
+          join ZÉID now
+        </h1>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(HandleRegister)} className="py-3">
+            {/* NAME INPUT */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="px-3">
+                  <FormLabel />
+                  <FormControl>
+                    <Input
+                      type="text"
+                      placeholder="NAME"
+                      {...field}
+                      className="rounded-none font-semibold  text-[#FFFFE3] outline-[#FFFFE3] text-center"
+                    />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* EMAIL */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="px-3">
+                  <FormLabel />
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="EMAIL"
+                      {...field}
+                      className="rounded-none font-semibold  text-[#FFFFE3] outline-[#FFFFE3] text-center"
+                    />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* PASSWORD */}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="px-3">
+                  <FormLabel />
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="PASSWORD"
+                      {...field}
+                      className="rounded-none font-semibold  text-[#FFFFE3] outline-[#FFFFE3] text-center"
+                    />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* REPASSWORD */}
+            <FormField
+              control={form.control}
+              name="rePassword"
+              render={({ field }) => (
+                <FormItem className="px-3">
+                  <FormLabel />
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="REPASSWORD"
+                      {...field}
+                      className="rounded-none font-semibold  text-[#FFFFE3] outline-[#FFFFE3] text-center"
+                    />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* PHONE */}
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem className="px-3">
+                  <FormLabel />
+                  <FormControl>
+                    <Input
+                      type="phone"
+                      placeholder="PHONE"
+                      {...field}
+                      className="rounded-none font-semibold  text-[#FFFFE3] outline-[#FFFFE3] text-center"
+                    />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="px-3">
+              <Button
+                type="submit"
+                className="rounded-none font-mono text-base sm:text-lg lg:text-xl font-bold w-full mt-2 sm:mt-2 py-2 sm:py-5 text-center bg-[#EA301F] text-[#FFFFE3] cursor-pointer hover:bg-[#FFFFE3] hover:text-[#000000] transition-all duration-400 uppercase "
+              >
+                Submit
+              </Button>
+            </div>
+            <div>
+              <p className="font-mono capitalize text-sm text-[#FFFFE3] text-center mt-2">
+                already have an account?{" "}
+                <Link href="/Login" className="text-[#EA301F]">
+                  Login
+                </Link>
+              </p>
+            </div>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 }
