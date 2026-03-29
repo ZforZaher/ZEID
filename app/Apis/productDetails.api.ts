df2ddfd706fb79a8f8a5dfc326762465ca90cd0d
Zaher <168756168+ZforZaher@users.noreply.github.com>
2026-03-29 14:40:26 +0200

    Fix missing newline at end of productDetails.api.ts

--- app/Apis/productDetails.api.ts
+++ app/Apis/productDetails.api.ts
@@ -2,4 +2,4 @@ export default async function getProductDetails(id:string){
     let response = await fetch(`https://ecommerce.routemisr.com/api/v1/products/${id}`)
     let data = response.json()
     return data
-}
+} 
