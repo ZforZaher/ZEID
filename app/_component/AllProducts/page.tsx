9c12fb5296b3867aa31a1b019c7f309bca218e78
Zaher <168756168+ZforZaher@users.noreply.github.com>
2026-03-29 14:50:38 +0200

    Update allproducts page.tsx

--- app/_component/AllProducts/page.tsx
+++ app/_component/AllProducts/page.tsx
@@ -5,7 +5,7 @@ import { Iproduct } from "../../interface/Product.interface";
 export default async function allProducts() {
     let {data} = await GetAllProducts()
     let Products = data
-  return (
+  return ( 
     <div className=" flex flex-wrap px-1">
             {Products.map((product:Iproduct)=>(
               <SingleProduct key={product._id} product={product} ></SingleProduct>
