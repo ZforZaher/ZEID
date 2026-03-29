1e54365a1a1fd76e21302981b0b926be90bd5428
Zaher <168756168+ZforZaher@users.noreply.github.com>
2026-03-29 15:28:24 +0200

    Update getCartProducts.ts

--- app/Apis/getCartProducts.ts
+++ app/Apis/getCartProducts.ts
@@ -1,6 +1,7 @@
 "use server"
 import getMyToken from "../../utilities/getMyToken";
 
+
 export default async function getCartProducts() {
   let token = await getMyToken();
   const headers: any = {
