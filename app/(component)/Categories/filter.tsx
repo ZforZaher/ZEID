46c949ec9a62c644785bed0ae9a50c6613934bb5
Zaher <168756168+ZforZaher@users.noreply.github.com>
2026-03-29 14:34:18 +0200

    Change initial state of isOpen to true

--- app/(component)/Categories/filter.tsx
+++ app/(component)/Categories/filter.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 
 export default function Filter() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   return (
     <>
