2eae779b31969a2c9777aff7e3c98ce62ae84328
Zaher <168756168+ZforZaher@users.noreply.github.com>
2026-03-29 15:35:38 +0200

    Update getMyToken.ts

--- utilities/getMyToken.ts
+++ utilities/getMyToken.ts
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 import { decode } from 'next-auth/jwt';
 
+
 export default async function getMyToken(){
     let decodedToken = (await cookies()).get(`next-auth.session-token`)?.value
     let token = await decode({token:decodedToken,secret:process.env.NEXTAUTH_SECRET!})
